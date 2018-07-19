a047c0e14176fd86c9a6a482acac4f3d527969a2
elenigeorgiou <helenig12@gmail.com>
2018-07-19 11:19:49 -0400

    segment control, ingredients on recipe card, and COLORS

--- MealPlanner/MealPlanner/RecipeViewController.swift
+++ MealPlanner/MealPlanner/RecipeViewController.swift
@@ -12,46 +12,63 @@ import UIKit
 class RecipeViewController : UIViewController {
     var chosenMeals : Meal = Meal()
     var mealChosen : String = "" //default
-    let instructionsData = ["Scrambled Egg Sandwich": "1.Scramble 1 egg in a pan coated with cooking spray \n2. Place egg between 2 slices 100% whole wheat bread \n3. Serve with 1/2 cup strawberries and black coffee", "Tuna Fish Melt Quesadilla"  : "1.In a bowl, combine tuna, mayonnaise, mustard and onion.\n2. Place a heavy bottom skillet over medium heat. \n3. Place tomato slices on one half of wrap. Spread tuna mixture over tomatoes and top with mozzarella (the other half of the wrap will have nothing on it). \n4. Fold wrap in half and brush with olive oil. \n5. Place wrap in pan and cook until golden brown, about 3 minutes. Flip and cook 2 more minutes. Serve. \n", "Herb-Crusted Salmon" : "1. Preheat oven to 425°.\n2. Season salmon with oil, 1⁄8 teaspoon salt and black pepper to taste.\n3. Place salmon on a sheet tray and top with herbs.\n4. In a medium bowl, thoroughly combine cracker crumbs, eggs and remaining 1⁄8 teaspoon salt and additional black pepper to taste.\n5. Place cracker crumb mixture on top of herbs and pat to make a crust.\n6. Place sheet tray in oven and bake until there is just a little bit of pink in the middle of the salmon, about 5 minutes depending on the thickness of the fish. Turn oven to broil and cook for an additional 2 minutes to brown crust, taking care not to burn. Serve with a wedge of lemon." ]
-    let nutritionData = ["Scrambled Egg Sandwich" : ["protein": "10 g", "carbs":"34 g", "fat": "5 g", "calories":"250 kcal"], "Tuna Fish Melt Quesadilla" : ["protein": "44 g", "carbs":"29 g", "fat": "12 g", "calories":"405 kcal"], "Herb-Crusted Salmon" : ["protein": "29 g", "carbs":"14 g", "fat": "9 g", "calories":"253 kcal"]]
-    let imageData = ["Scrambled Egg Sandwich" : "eggs.jpg", "Tuna Fish Melt Quesadilla": "tuna.jpg", "Herb-Crusted Salmon": "salmon.jpg"]
-  //  let nutritionLabel = UILabel(accessibilityIdentifier:"nutrition")
-
+    var ingredients: String = ""
     @IBOutlet weak var recipeLabel: UILabel!
     @IBOutlet weak var image: UIImageView!
   
+    @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var instructionLabel: UITextView!
+    
+    @IBOutlet weak var segmentLabel: UILabel!
+    @IBAction func segementControlSwitch(_ sender: Any) {
+        
+        switch segmentControl.selectedSegmentIndex{
+            case 0:
+            //textLabel.text = "First Segment Selected";
+                segmentLabel.text = "Nutrition"
+                self.view.addSubview(segmentLabel)
+                let protein : String! = String(chosenMeals.protein!)
+                let carbs : String! = String(chosenMeals.carbs!)
+                let fats : String! = String(chosenMeals.fat!)
+                let cals : String! = String(chosenMeals.calories!)
+                instructionLabel.text = "Protein: \(protein!) grams \nCarbs: \(carbs!) grams \nFats: \(fats!) grams \nCalories: \(cals!) calories"
+                self.view.addSubview(instructionLabel)
+            case 1:
+                segmentLabel.text = "Instructions"
+                self.view.addSubview(segmentLabel)
+                instructionLabel.text = chosenMeals.instructions
+                self.view.addSubview(instructionLabel)
+            case 2:
+                segmentLabel.text = "Ingredients"
+                self.view.addSubview(segmentLabel)
+                instructionLabel.text = ingredients
+                self.view.addSubview(instructionLabel)
+            default:
+            break
+        }
+    }
     
     @IBOutlet weak var nutritionLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        //let recipe : String! = mealChosen
-       // print(recipe)
         let recipe : String! = chosenMeals.name
         print(recipe)
-       // recipeLabel.text = recipe
         recipeLabel.text = recipe
-      //  instructionLabel.text = instructionsData[recipe]
+        
         let  protein : String! = String(chosenMeals.protein!)
         let carbs : String! = String(chosenMeals.carbs!)
         let fats : String! = String(chosenMeals.fat!)
         let cals : String! = String(chosenMeals.calories!)
-        instructionLabel.text = chosenMeals.instructions
-//        let  protein : String! = nutritionData[recipe]!["protein"]
-//        let carbs : String! = nutritionData[recipe]!["carbs"]
-//        let fats : String! = nutritionData[recipe]!["fat"]
-//        let cals : String! = nutritionData[recipe]!["calories"]
-        nutritionLabel.text = "Protein: \(protein!) grams \nCarbs: \(carbs!) grams \nFats: \(fats!) grams \nCalories: \(cals!) calories"
+        instructionLabel.text = "Protein: \(protein!) grams \nCarbs: \(carbs!) grams \nFats: \(fats!) grams \nCalories: \(cals!) calories"
+        for ing in chosenMeals.ingredients!{
+            ingredients = "\(ingredients) \(ing.quantity!)  \(ing.name!)\n"
+        }
         image.image = UIImage(named: chosenMeals.image!)
-        
-        // image.image = UIImage(named: imageData[recipe]!)
         self.view.addSubview(image)
         self.view.addSubview(recipeLabel);
-        self.view.addSubview(nutritionLabel)
         self.view.addSubview(instructionLabel)
         
     }
-    
     
     
     
